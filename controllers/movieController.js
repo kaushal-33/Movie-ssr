7cb2ad6e5fb2a5c77e93b42c9aa487c8e7d8ce7f
kaushal-33 <varmakaushal33@gmail.com>
2025-12-12 11:28:02 +0530

    added delete function and some UI

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,19 +1,51 @@
+const { name } = require("ejs");
 const MovieModel = require("../models/movieSchema");
 
-exports.adminMovies = (req, res) => {
-    return res.render("adminIndex");
+let movies = [{
+    movieName: "dhurandar",
+    genre: "action",
+    date: "2 feb",
+    rating: "4",
+    posterURL: "images/Poster-Dhurandhar-2025.webp",
+    detail: "action comedy"
+
+}];
+
+exports.adminMovies = async (req, res) => {
+    try {
+        const movieArr = await MovieModel.find();
+        console.log(movieArr);
+        return res.render("adminIndex", { movies, movieArr });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 exports.movieRoutes = (req, res) => {
     return res.render("index");
 }
 
+exports.movieForm = (req, res) => {
+    return res.render("form");
+}
+
 exports.addMovie = async (req, res) => {
     try {
-        console.log(req.file);
-        const movieObj = new MovieModel({ ...req.body, photoURL: req.file.path });
+        // console.log(req.file);
+        const movieObj = new MovieModel({ ...req.body, posterURL: req.file.path });
         await movieObj.save();
+        return res.redirect("/admin");
     } catch (error) {
         console.log(error);
+    }
+}
+
+exports.deleteMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await MovieModel.findByIdAndDelete(id);
+        return res.redirect("/admin");
+    } catch (error) {
+        console.log(error)
     }
 }
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { adminMovies, addMovie } = require("../controllers/movieController");
+const { adminMovies, addMovie, movieForm, deleteMovie } = require("../controllers/movieController");
 const upload = require("../middlewares/multer");
 const router = express.Router();
 
 router.get("/", adminMovies);
 router.post("/add-movie", upload.single("posterURL"), addMovie)
+router.get("/add-movie", movieForm);
+router.get("/delete-movie/:id", deleteMovie);
 
 module.exports = router;
